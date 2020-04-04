75b59b51b61a36349a1201c498fd9e1265010e4b
isaac <isaac.ingles@gmail.com>
2020-04-04 11:46:58 -0600

    initial commit

---
+++ cardBlock.js
@@ -0,0 +1,30 @@
+const template = document.createElement('template')
+template.innerHTML = `
+<style>
+    .card-block {
+        display: inline-block;
+        border: 1px solid #ccc;
+        padding: 1rem;
+
+        transition: background-color 1s ease-out;
+    }
+
+    .card-block:hover {
+        background: #ccccff;
+    }
+</style>
+<div class="card-block">
+    <slot />
+</div>
+`
+
+class CardBlock extends HTMLElement {    
+
+    constructor() {
+        super()
+        this.attachShadow({ mode: 'open' })
+        this.shadowRoot.appendChild(template.content.cloneNode(true))
+    }
+}
+
+window.customElements.define('card-block', CardBlock)
